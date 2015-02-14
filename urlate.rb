fe910fef20665d3a11994a83f3d138ea5437721e
Quinn Avila <qavila@drw.com>
2015-02-14 10:59:12 -0600

    added login page

--- urlate.rb
+++ urlate.rb
@@ -1,11 +1,23 @@
 require 'sinatra'
 
+enable :sessions
+
 get '/' do
   erb :index
 end
 
 get '/admin' do
-  erb :admin
+  erb :"admin/admin"
+end
+
+get '/admin/login' do
+  erb :"admin/admin_login"
+end
+
+post '/admin/login' do
+  session[:username] = params[:username]
+  params.inspect
+  redirect "/admin"
 end
 
 get '/users/:user' do
